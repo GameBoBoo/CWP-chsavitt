5a1fe1cb972da43d7b9fcbb2de8afa0e8920fe56
GameBoBoo <Chawalertsavittrakul@gmail.com>
2026-09-21 19:38:24 +0700

    add Python scripts for exercises in discovery piscine

---
+++ discovery_piscine/cell07/ex00/your_namebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def array_of_names(persons):
+    result = []
+    for first, last in persons.items():
+        full_name = f"{first.capitalize()} {last.capitalize()}"
+        result.append(full_name)
+    return result
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))
